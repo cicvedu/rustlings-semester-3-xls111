e921880ba5b664570d6fa7cbd39e049f2c9c69f0
DarkFlamer <chinaczq519@163.com>
2024-03-24 20:18:02 +0800

    my first answer

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -3,10 +3,11 @@
 // Execute `rustlings hint errors4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
+
+
 
 #[derive(PartialEq, Debug)]
 enum CreationError {
@@ -17,7 +18,11 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+        match value {
+            v if v > 0 => Ok(PositiveNonzeroInteger(v as u64)),
+            0 => Err(CreationError::Zero),
+            _  => Err(CreationError::Negative), 
+        }
     }
 }
 
--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,11 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -39,6 +39,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+       
+        // Update the goals for team 1
+        scores.entry(team_1_name.clone()).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        }).goals_scored += team_1_score;
+        scores.get_mut(&team_1_name).unwrap().goals_conceded += team_2_score;
+
+        // Update the goals for team 2
+        scores.entry(team_2_name.clone()).or_insert(Team {
+            goals_scored: 0,
+            goals_conceded: 0,
+        }).goals_scored += team_2_score;
+        scores.get_mut(&team_2_name).unwrap().goals_conceded += team_1_score;
+        
     }
     scores
 }
@@ -59,6 +74,7 @@ mod tests {
     #[test]
     fn build_scores() {
         let scores = build_scores_table(get_results());
+        println!("{:?}",scores);
 
         let mut keys: Vec<&String> = scores.keys().collect();
         keys.sort();
